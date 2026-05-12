a875c2898907892e40464f37b7c307cd78c0a96b
Yu_Dev <seungsu97233@gmail.com>
2026-05-12 14:08:20 +0200

    tableau

--- src/Exercices/tableau.js
+++ src/Exercices/tableau.js
@@ -1,9 +1,9 @@
-console.log("helloWorld")
+console.log("helloWorld");
 let mesNombres = [100,200,300];
 let name = "Được Chúa ban phước";
 let age = "25"
 let sport = ["Basketball","Piano","caligrafie"]
-let tabUser = [nom,age,sport]
+let tabUser = [name,age,sport]
 
 console.log(tabUser);
 console.log(tabUser[2])
