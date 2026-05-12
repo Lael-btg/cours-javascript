4aae8c1fb740d6de3d412d734e5cc9dbda7bebfa
Yu_Dev <seungsu97233@gmail.com>
2026-05-12 16:54:36 +0200

    update

---
+++ src/Exercices/fonction.js
@@ -0,0 +1,20 @@
+let name = "Được Chúa ban phước"
+let age = 25
+let passion = ["Basketball","Piano","calligrafie"]
+
+let a = 7
+let b = 24
+let c = 1
+let fx = 
+console.log( a + b );
+console.log("hello " + "everyone");
+console.log(name+age)
+function additioner (nombre=O) {
+    console.log(33+17)
+}
+additioner(3)
+
+function additioner2(nombre1=0, nombre2=0) {
+    console.log(33+2)
+}
+additioner2(17,22)
--- src/Exercices/tableau.js
+++ src/Exercices/tableau.js
@@ -1,11 +1,34 @@
 console.log("helloWorld");
 let mesNombres = [100,200,300];
 let name = "Được Chúa ban phước";
-let age = "25"
-let sport = ["Basketball","Piano","caligrafie"]
-let tabUser = [name,age,sport]
+let yo = "25"
+let sport = ["Basketball","Piano","calligrafie"]
 
+//correctiion
+// Exercice 1
+let prenom = 'JoSé';
+let age = 45;
+//! On déclare un tableau avec cette syntaxe : []
+//! On peut placer ce que l'on veut dans un tableau 
+// let unTableau = [12,'Salut ca va bien?',prenom,age];
+// console.log(unTableau);
+// console.log(unTableau[2]);
+//! Exemple avec un tableau dans un tableau 
+let mesPassions = ["Boxe","Jonquilles"];
+let tabUser = [prenom, age, mesPassions];
 console.log(tabUser);
-console.log(tabUser[2])
+console.log(tabUser[2]);
+console.log(tabUser[2][1]);
 
 
+// Exercice 2
+let testTabAjout = [10,10,10];
+console.log(`Avant Ajout :',${testTabAjout}`);
+testTabAjout.push('Cortex',92,'Les Pyramides');
+console.log('Après Ajout : ',testTabAjout);
+
+//Exemple d'utilisation de .pop pour supprimer le dernier élélments d'un tableau 
+let testTabSuppression = [10,10,10];
+console.log('Avant Suppression :',testTabSuppression);
+testTabSuppression.pop();
+console.log('Après Suppression : ',testTabSuppression);
