8f44714d6d75c4d79404950706aaa2152da54c57
Yu_Dev <seungsu97233@gmail.com>
2026-05-13 13:23:16 +0200

    tibo est pas très bon

--- src/Exercices/tibo.js
+++ src/Exercices/tibo.js
@@ -1,8 +1,22 @@
+
 const nombres = [0, 3, 4, 6, 18, 19, 4];
 const moyenne = nombres.reduce(
     (accumulateur, nombreCourant) => accumulateur + nombreCourant,
     0
 ) / nombres.length;
 console.log('la moyenne de tibo est de:', moyenne)
+
+
+function testNum(moyenne) {
+    let result;
+if (moyenne => 5) {
+    result = "Bad Boy";
+    } else {
+    result = "Good Boy";
+    }
+return result;
+}
+
+console.log(testNum(moyenne));
 
 
